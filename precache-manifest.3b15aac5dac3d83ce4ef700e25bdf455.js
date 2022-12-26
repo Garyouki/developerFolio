a6133654920742b2dd1182aecd2000406bba58b1
Garyouki <Garyouki@users.noreply.github.com>
2022-12-26 12:32:43 +0000

    Deploying to gh-pages from  @ 352d0a720cf13a396381e8950ed010787c0a90d9 🚀

---
+++ precache-manifest.3b15aac5dac3d83ce4ef700e25bdf455.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe9e9ef93b374efdd50773a00a234e43",
+    "url": "/Garyouki.github.io/developerFolio/index.html"
+  },
+  {
+    "revision": "66bce8cdcc873c4ee172",
+    "url": "/Garyouki.github.io/developerFolio/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "bce0c001207de696b36f",
+    "url": "/Garyouki.github.io/developerFolio/static/css/4.d7ceb645.chunk.css"
+  },
+  {
+    "revision": "b544e1c8ed2d50e1c98b",
+    "url": "/Garyouki.github.io/developerFolio/static/css/main.73a5d72b.chunk.css"
+  },
+  {
+    "revision": "078f895ce8bde5aa6ac4",
+    "url": "/Garyouki.github.io/developerFolio/static/js/2.81e28692.chunk.js"
+  },
+  {
+    "revision": "408f8ade359af8695a328ed937291fdf",
+    "url": "/Garyouki.github.io/developerFolio/static/js/2.81e28692.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66bce8cdcc873c4ee172",
+    "url": "/Garyouki.github.io/developerFolio/static/js/3.39e4d19b.chunk.js"
+  },
+  {
+    "revision": "bce0c001207de696b36f",
+    "url": "/Garyouki.github.io/developerFolio/static/js/4.ec2ecc6b.chunk.js"
+  },
+  {
+    "revision": "b544e1c8ed2d50e1c98b",
+    "url": "/Garyouki.github.io/developerFolio/static/js/main.be0eb968.chunk.js"
+  },
+  {
+    "revision": "df73b4fa8cff46840155",
+    "url": "/Garyouki.github.io/developerFolio/static/js/runtime-main.63aa6276.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/Garyouki.github.io/developerFolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/Garyouki.github.io/developerFolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "1973cb8fb4e99f4f9dd752d6ef8064f0",
+    "url": "/Garyouki.github.io/developerFolio/static/media/Washington_University.1973cb8f.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/Garyouki.github.io/developerFolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/Garyouki.github.io/developerFolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "4759b5daf8baa24cfa703983a76ce30b",
+    "url": "/Garyouki.github.io/developerFolio/static/media/food.4759b5da.jpeg"
+  },
+  {
+    "revision": "77086926c91590ad6dc4470a1029af39",
+    "url": "/Garyouki.github.io/developerFolio/static/media/g.77086926.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/Garyouki.github.io/developerFolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "4b2938b9f5ea2f1ddbd45dccaff628bc",
+    "url": "/Garyouki.github.io/developerFolio/static/media/quoraLogo.4b2938b9.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/Garyouki.github.io/developerFolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/Garyouki.github.io/developerFolio/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
